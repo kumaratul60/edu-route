dd5127afb756e74e3807ac219546d0d1a927cec8
samHack03 <sharma.sumant391@gmail.com>
2023-01-29 01:26:41 +0530

    added Image Link in competitive

--- src/categories/Competative.js
+++ src/categories/Competative.js
@@ -4,86 +4,100 @@ export const exams = [
         name: "Civil Services Examination (CSE)",
         description:
             "The Civil Services Examination (CSE) is conducted by the Union Public Service Commission (UPSC) to recruit candidates for the Indian Administrative Service (IAS), Indian Police Service (IPS), and Indian Foreign Service (IFS), among other services. It is considered one of the toughest and most prestigious exams in India.",
+            img:"https://s3-ap-southeast-1.amazonaws.com/subscriber.images/free-ias-prep/2016/12/24065818/Civil-Services-Examination.jpg"
     },
     {
         id: 2,
         name: "Graduate Aptitude Test in Engineering (GATE)",
         description:
             "The GATE (Graduate Aptitude Test in Engineering) exam is conducted by the Indian Institute of Science (IISc) and the Indian Institutes of Technology (IITs) for admissions to postgraduate engineering programs in Indian institutes of higher education. It is considered to be highly competitive and is open to engineering graduates and diploma holders. The exam tests the candidate's knowledge in various branches of engineering such as mechanical, electrical, civil, computer science, and electronics. A good score in GATE is also considered for recruitment in government organizations such as Indian Oil Corporation Limited, Bharat Heavy Electrical Limited, and National Thermal Power Corporation.",
+            img:"https://tse1.mm.bing.net/th?id=OIP.ygvuha5WkFWGvVYbTun-_wHaE0&pid=Api&P=0"
     },
     {
         id: 3,
         name: "Union Public Service Commission (UPSC)",
         description:
             "The Indian Administrative Service (IAS),Indian Police Service (IPS),Indian Foreign Service (IFS),Indian Revenue Service (IRS), Indian Economic Service (IES) and Indian Statistical Service (ISS) are conducted by the Union Public Service Commission (UPSC) to recruit candidates. These exams are highly competitive and are considered one of the toughest exams in India.",
+        img:"https://2.bp.blogspot.com/-EkBSxFq-mxc/WU-HA_7A6OI/AAAAAAAACt8/cv39lN8FQBkkIPQqWwh_EZ9PEVBAMF0OACPcBGAYYCw/s1600/UPSC-logo.jpg"
     },
     {
         id: 4,
         name: "Indian Post Graduate Medical Entrance Examination (AIPGMEE)",
         description:
             "The All India Post Graduate Medical Entrance Examination (AIPGMEE) is conducted by the National Board of Examinations (NBE) to recruit candidates for postgraduate medical courses in India. It is a highly competitive exam and is considered one of the toughest exams in India.",
-    },
+        img:"http://www.italcoholic.in/wp-content/uploads/2016/07/AIPGMEE-2016-Exam-Dates-Announced.jpg"
+        },
     {
         id: 5,
         name: "National Eligibility cum Entrance Test (NEET)",
         description:
             "The National Eligibility cum Entrance Test (NEET) is conducted by the National Testing Agency (NTA) to recruit candidates for undergraduate medical courses in India. It is a highly competitive exam and is considered one of the toughest exams in India.",
-    },
+        img:"https://timesofindia.indiatimes.com/photo/72073355.cms"
+        },
     {
         id: 6,
         name: "Chartered Accountancy (CA)",
         description:
             "The Chartered Accountancy (CA) exam is conducted by the Institute of Chartered Accountants of India (ICAI) to recruit candidates for the Chartered Accountancy (CA) course. It is a highly competitive exam and is considered one of the toughest exams in India.",
-    },
+        img:"https://tse1.mm.bing.net/th?id=OIP.8tciIJEQ1gbkwiE-GWDyGAHaDJ&pid=Api&P=0"
+        },
     {
         id: 7,
         name: "Company Secretary (CS)",
         description:
             "The Company Secretary (CS) exam is conducted by the Institute of Company Secretaries of India (ICSI) to recruit candidates for the Company Secretary (CS) course. It is a highly competitive exam and is considered one of the toughest exams in India.",
-    },
+        img:"https://tse3.mm.bing.net/th?id=OIP.dK_SMdrY5ckKUJOzNWZJQAAAAA&pid=Api&P=0"
+        },
     {
         id: 8,
         name: "Indian Institute of Management (IIM) Common Admission Test (CAT)",
         description:
             "The Indian Institute of Management (IIM) Common Admission Test (CAT) is conducted by the Indian Institute of Management (IIM) to recruit candidates for MBA",
-    },
+        img:"https://hindisahayta.in/wp-content/uploads/2018/10/cat-exam.jpg"
+        },
     {
         id: 9,
         name: "Staff Selection Commission (SSC) ",
         description:
             " The SSC conducts various competitive exams such as the Combined Graduate Level Examination, Junior Engineer Examination, Multi-Tasking (Non-Technical) Staff Examination, and Stenographer Examination. These exams are considered to be highly competitive and are open to graduates and diploma holders.",
-    },
+        img:"https://www.edubull.com/images/blog/ssc-exam.jpg"
+        },
     {
         id: 10,
         name: "Institute of Banking Personnel Selection (IBPS) ",
         description:
             " The IBPS conducts various competitive exams such as the Probationary Officer Examination, Specialist Officer Examination, and Clerk Examination. These exams are considered to be highly competitive and are open to graduates and diploma holders.",
-    },
+        img:"http://bankexamportal.com/sites/default/files/IBPS-PO-LOGO.jpeg"
+        },
     {
         id: 11,
         name: "Reserve Bank of India (RBI) ",
         description:
             "The RBI conducts various competitive exams such as the Grade B Officer Examination, Assistant Examination, and Officer in Grade ‘C’ Examination. These exams are considered to be highly competitive and are open to graduates and diploma holders.",
-    },
+        img:"https://2.bp.blogspot.com/-fihXhTUhwos/Ud_96EeT7OI/AAAAAAAAOt0/phtY5votxtQ/s1600/RBI+Assistants+exam+call+letter+download.gif"
+        },
     {
         id: 12,
         name: "National Defense Academy (NDA)",
         description:
             "The NDA exam is conducted by the Union Public Service Commission (UPSC) for recruiting officers in the Indian Army, Navy, and Air Force. This exam is considered to be highly competitive and is open to candidates who have completed their 12th standard.",
-    },
+        img:"https://images.indianexpress.com/2021/03/NDA-1200-2.jpg"
+        },
 
     {
         id: 13,
         name: "Combined Defense Services (CDS)",
         description:
             " The CDS exam is conducted by the Union Public Service Commission (UPSC) for recruiting officers in the Indian Army, Navy, and Air Force. This exam is considered to be highly competitive and is open to graduates and diploma holders.",
-    },
+        img:"https://upscbuddy.com/wp-content/uploads/2019/01/CDS-Exam.jpg"
+        },
     {
         id: 14,
         name: "National Defense Academy (NDA)",
         description:
             "The NDA exam is conducted by the Union Public Service Commission (UPSC) for recruiting officers in the Indian Army, Navy, and Air Force. This exam is considered to be highly competitive and is open to candidates who have completed their 12th standard.",
-    },
+        img:"https://www.entranceexams.io/wp-content/uploads/2017/04/nda.png"
+        },
 
 
     {
@@ -91,17 +105,20 @@ export const exams = [
         name: "Joint Admission Test for M.Sc. (JAM)",
         description:
             "The JAM exam is conducted by the Indian Institutes of Technology (IITs) for admissions to postgraduate M.Sc. programs in various branches of science and technology. This exam is considered to be highly competitive and is open to undergraduate students in science and technology disciplines.",
-    },
+        img:"https://i1.wp.com/caknowledge.com/wp-content/uploads/2018/05/JAM.jpg?fit=820%2C461&ssl=1"
+        },
     {
         id: 16,
         name: "Junior Research Fellowship (JRF)",
         description:
             "The JRF exam is conducted by the Council of Scientific and Industrial Research (CSIR) for selecting candidates for the award of Junior Research Fellowship in various fields of science and technology. This exam is considered to be highly competitive and is open to postgraduate students in science and technology disciplines.",
-    },
+        img:"https://3.bp.blogspot.com/-aQrH-x0yFg8/Wp1IDoJpkcI/AAAAAAAAO1g/auMr4mMjvvcddP3AxThgALdWx4z-C5hkACK4BGAYYCw/w1200-h630-p-k-no-nu/CSIR%2BUGC%2BJRF%2Band%2BLS%2BNET%2BExam%2BJune%2B2019.png"
+        },
     {
         id: 17,
         name: "National Eligibility Test (NET) Exam",
         description:
             "The NET exam is conducted by the National Testing Agency (NTA) for determining the eligibility of Indian nationals for the post of Assistant Professor or Junior Research Fellowship (JRF) in Indian universities and colleges. This exam is considered to be highly competitive and is open to postgraduate students in various disciplines such as science, humanities, and social sciences.",
-    },
+        img:"https://i.ytimg.com/vi/aLXz2Y0iH0A/hqdefault.jpg"
+        },
 ];
